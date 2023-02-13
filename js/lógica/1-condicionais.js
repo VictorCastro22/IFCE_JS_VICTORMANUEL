af7715b4da3ea5343a0befaf852e6756091aaf5a
Victor <victor3mcastro@gmail.com>
2023-02-13 16:49:22 -0300

    Seção 02 e 03 finalizadas

--- js/lógica/1-condicionais.js
+++ js/lógica/1-condicionais.js
@@ -1,35 +1,14 @@
-console.log("Destinos possíveis:");
-console.log(listaDeDestinos);
-
-// if (idadeComprador >= 18) {
-//     console.log("Comprador maior de idade");
-//     listaDeDestinos.splice(1, 1); //removendo item
-// } else if (estaAcompanhada == true) {
-//     console.log("Comprador está acompanhado");
-//     listaDeDestinos.splice(1, 1); //removendo item
-// } else {
-//     console.log("Não é maior de Idade e não posso vender");
-// }
-
-if (idadeComprador >= 18 || estaAcompanhada == true) {
-    console.log("Boa Viagem!!");
-    listaDeDestinos.splice(2, 1); //removendo item
-} else {
-    console.log("Não é maior de Idade e não posso vender");
+// Escreva uma função que receba dois números e retorne o maior valor.
+function retornaMaior (x, y) {
+    if (x > y) {
+        console.log("O maior valor é", x)
+    }
+    else if(x < y) {
+        console.log("O maior valor é", y);
+    }
+    else{
+        console.log("Os valores são iguais");
+    }
 }
 
-console.log("Embarque: \n\n")
-if(idadeComprador >= 18 && temPassagemComprada){
-    console.log("Boa viagem");
-}else{
-    console.log("Você não pode embarcar");
-}
-
-
-console.log(listaDeDestinos);
-
-// console.log(idadeComprador > 18);
-// console.log(idadeComprador < 18);
-// console.log(idadeComprador <= 18);
-// console.log(idadeComprador >= 18);
-// console.log(idadeComprador == 18);
+retornaMaior(15, 15);
