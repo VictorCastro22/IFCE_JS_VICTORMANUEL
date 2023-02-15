98a5b379ed5ede96c4350abe76d7ce04e30b765b
Victor <victor3mcastro@gmail.com>
2023-02-15 14:49:35 -0300

    Funções imediatas (IIFE)

--- js/secao04.js
+++ js/secao04.js
@@ -40,3 +40,21 @@ usaFalaNome();*/
 //AULA -  Closures > Habilidade de acessar o escopo léxico
 
 //AULA - Funções de callback > Ordenar a maneira que as funções serão executadas
+
+/*AULA - Funções imediatas (IIFE)
+Fazer com que as variáveis e funções venham a fugir do escopo global
+(function(idade, peso, altura) {
+    
+    const sobrenome = "Miranda";
+    function criaNome(nome) {
+        return nome + " " + sobrenome;
+    }
+
+    function falaNome() {
+        console.log(criaNome("Luiz"));
+    }
+
+    falaNome();
+    console.log(idade, peso, altura);
+
+})(30, 80, 1.80);*/
