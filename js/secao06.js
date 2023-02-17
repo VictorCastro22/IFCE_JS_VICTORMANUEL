4a6eaaae0dd4ef0d55debdb78560b7c13ff6acfc
Victor <victor3mcastro@gmail.com>
2023-02-17 15:42:30 -0300

    Revisando Objetos

---
+++ js/secao06.js
@@ -0,0 +1,15 @@
+
+/*AULA -  Revisando Objetos
+// Factory functions / Constructor functions / Classes
+function Pessoa(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+  
+    Object.freeze(this); //Trava objeto
+  }
+  // {} <- this
+  const p1 = new Pessoa('Luiz', 'Miranda');//Construtora
+  const p2 = new Pessoa('Maria', 'Miranda');//Costrutora
+  
+  console.log(p1);
+  console.log(p2); */
