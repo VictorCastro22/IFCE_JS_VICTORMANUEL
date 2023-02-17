245bae500d84ff71984be388fcc661265a76d633
Victor <victor3mcastro@gmail.com>
2023-02-17 16:18:52 -0300

    Object.defineProperty() e Object.defineProperties()

--- js/secao06.js
+++ js/secao06.js
@@ -13,3 +13,24 @@ function Pessoa(nome, sobrenome) {
   
   console.log(p1);
   console.log(p2); */
+
+/*AULA - Object.defineProperty() e Object.defineProperties()
+// defineProperty -> Getter e Setters
+function Produto(nome, preco, estoque) {
+    this.nome = nome;
+    this.preco = preco;
+  
+    Object.defineProperty(this, 'estoque', {
+      enumerable: true, // mostra a chave
+      value: estoque, // valor
+      writable: true, // pode alterar
+      configurable: true // configurável
+    });
+  }
+  
+  const p1 = new Produto('Camiseta', 20, 3);
+  console.log(Object.keys(p1));
+  
+  for(let chave in p1) {
+    console.log(chave);
+  } */
