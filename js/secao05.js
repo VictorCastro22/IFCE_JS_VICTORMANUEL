3cb574f8c3a90fe322b588d3ea45fd6845018a72
Victor <victor3mcastro@gmail.com>
2023-02-17 14:26:31 -0300

    Filter - Filtrando o array

--- js/secao05.js
+++ js/secao05.js
@@ -44,3 +44,45 @@ const a3 = a1.concat(a2, [7, 8, 9], 'Luiz');
 // const a3 = [...a1, 'Luiz', ...a2, ...[7, 8, 9]];
 console.log(a1, a2);
 console.log(a3); */
+
+/*AULA -   Filter - Filtrando o array
+
+// Filter -> Sempre retornar um array, com a mesma quantidade de elementos
+// ou menos.
+//**Manda função de callback e vai reduzindo para uma function**
+// Retorne os números maiores que 10
+//               0  1   2   3  4 .....
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+
+function callbackFilter(valor){
+    if (valor > 10) {
+        return true;
+    } else {
+        return false;
+    }
+    //Posso simplificar a expressão acima apenas usando return?
+    return valor > 10;    
+}
+//Posso diminuir botando direto dentro do filter e fazendo como arrow function
+
+const numerosFiltrados = numeros.filter(valor => valor > 50);
+console.log(numerosFiltrados);
+
+/*Retorne as pessoas que tem o nome com 5 letras ou mais
+// Retorne as pessoas com mais de 50 anos
+// Retorne as pessoas cujo nome termina com a
+const pessoas = [
+  { nome: 'Luiz', idade: 62 },
+  { nome: 'Maria', idade: 23 },
+  { nome: 'Eduardo', idade: 55 },
+  { nome: 'Letícia', idade: 19 },
+  { nome: 'Rosana', idade: 32 },
+  { nome: 'Wallace', idade: 47 },
+];
+const pessoasComNomeGrande = pessoas.filter(obj => obj.nome.length >= 5);
+const pessoasComMaisDeCinquentaAnos = pessoas.filter(obj => obj.idade > 50);
+const nomeTerminaComA = pessoas.filter(obj => {
+  return obj.nome.toLowerCase().endsWith('a');
+});
+console.log(nomeTerminaComA);*/
+
