7d647f0120276e10c1adf9a4f70f5ede2ad6a804
Victor <victor3mcastro@gmail.com>
2023-02-17 13:51:03 -0300

    Método Splice

--- js/secao05.js
+++ js/secao05.js
@@ -1,4 +1,4 @@
-//AULA - Revisão do básico em Arrays
+/*AULA - Revisão do básico em Arrays
 //                 0         1        2        3          4
 // const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana'];
 // const novo = nomes.slice(1, -2);
@@ -15,4 +15,21 @@
 
 const nomes = [ 'Luiz', 'Otávio', 'Miranda' ];
 const nome = nomes.join(' ');
-console.log(nome);
+console.log(nome);*/
+
+/*AULA - Método Splice > Retorna um array
+//               -5       -4       -3         -2        -1
+//                0        1        2          3         4
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia'];
+
+// Unshift
+nomes.splice(2, 0, 'Luiz', 'Otávio');
+console.log(nomes);
+
+//.splice > faz quase tudo com a manipulação, pois simula alguns aspectos.
+// nomes.splice(índice atual, delete, elem1, elem2, elem3);
+// pop
+// const removidos = nomes.splice(-1, 1);
+
+// shift
+// const removidos = nomes.splice(0, 1);*/
