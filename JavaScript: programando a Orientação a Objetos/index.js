3709e537eb6c65bdfe9ae44225b2cbeeaa7dd02e
Victor <victor3mcastro@gmail.com>
2023-03-01 08:55:12 -0300

    Criando classes

--- JavaScript: programando a Orientação a Objetos/index.js
+++ JavaScript: programando a Orientação a Objetos/index.js
@@ -1,21 +1,38 @@
 class Cliente {
+
     nome;
     cpf;
-    agencia;
-    saldo;
+    
 }
 
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
+class ContaCorrente{
 
+    agencia;
+    saldo;
+
+    sacar(valor){
+        if(this.saldo <= valor){
+            this.saldo -= valor;
+        }
+    }
+
+    depositar(valor){
+        if(this.saldo > 0){
+            this.saldo += valor;
+        }
+    }
+
+}
+
+const cliente1 = new Cliente;
 cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
+cliente1.cpf = 0123456701;
 
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.saldo = 1;
 
-console.log(cliente1, cliente2);
+
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.depositar(50);
+console.log(contaCorrenteRicardo.saldo);
