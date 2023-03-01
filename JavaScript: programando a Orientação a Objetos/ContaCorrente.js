854495726e16fbf29996ea44568d73743a4bbd17
Victor Manuel <victor3mcastro@gmail.com>
2023-03-01 14:47:11 -0300

    Get e Set

--- JavaScript: programando a Orientação a Objetos/ContaCorrente.js
+++ JavaScript: programando a Orientação a Objetos/ContaCorrente.js
@@ -1,9 +1,26 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente{
 
     agencia;
     /*#saldo = 0; //Botando o atributo privado. */
     _saldo = 0; //Mostrandoao usuário que o campo é privado.
-    cliente;
+
+    get saldo() {
+        return this._saldo;
+    }
+    
+    _cliente;
+
+    set cliente(novoValor) { //Atribuição
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente() { //Retorna
+        return this._cliente;
+    }
 
     sacar(valor) {
         if (this._saldo >= valor) {
--- JavaScript: programando a Orientação a Objetos/index.js
+++ JavaScript: programando a Orientação a Objetos/index.js
@@ -17,13 +17,15 @@ contaCorrenteRicardo.depositar(500);
 
 
 const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
+conta2.cliente = cliente1;
 conta2.agencia = 102;
 
-contaCorrenteRicardo.transferir(200, conta2)
+let valor = 200;
+contaCorrenteRicardo.transferir(valor, conta2)
 
-console.log(contaCorrenteRicardo);
-console.log(conta2);
+
+/*conta2.saldo = 30000; //O get não vai permitir atribuições do tipo.*/
+console.log(conta2.saldo);
 
 /*console.log(contaCorrenteRicardo._saldo);
 contaCorrenteRicardo.depositar(100);
